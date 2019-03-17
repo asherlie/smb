3036cda80fb0b204f1613f50e698af558bb9275a
Asher Lieber <asherlie@buffalo.edu>
2019-03-16 20:33:30 -0400

    introduced any mode, associated with flag -any, which will join the first board that is found, even without any search term, -C flag is no longer required for board creation - if a board to join is not found, it will be created

--- smb.c
+++ smb.c
@@ -8,6 +8,7 @@
 #include "host.h"
 #include "client.h"
 
+
 /* 
  * a new mb can be created by running smb with 
  * the -C flag followed by board name
@@ -35,7 +36,7 @@ char* sc_dir(char* path, char* sterm, int extlen, char* ext){
       int namelen;
       _Bool good;
       while((inf = readdir(dir))){
-            if(strstr(inf->d_name, sterm)){
+            if(!sterm || strstr(inf->d_name, sterm)){
                   if(((int)(namelen = strnlen(inf->d_name, PATH_MAX)) <= extlen))
                         continue;
                   good = 1;
@@ -73,51 +74,69 @@ void p_usage(char* bname){
 }
 
 int main(int a, char** b){
-      /* TODO: should we join the first .smbr we can find in this case? */
-      if(a == 1){
+      _Bool lim_pwd = 0, create = 0, any = 0;
+      int cre_arg = -1;
+      for(int i = 1; i < a; ++i){
+            if(*b[i] == '-'){
+                  switch(b[i][1]){
+                        /* pwd mode */
+                        case 'p':
+                              lim_pwd = 1;
+                              break;
+                        /* host/create mode */
+                        case 'C':
+                              if(i == a-1)break;
+                              create = 1;
+                              cre_arg = i+1;
+                              break;
+                        case 'a':
+                              any = 1;
+                              break;
+
+                        /* cre_arg will be set to either to element after -C or
+                         * the first non flag
+                         */
+                        default:
+                              if(cre_arg == -1)
+                                    cre_arg = i;
+                  }
+            }
+            else if(cre_arg == -1)
+                  cre_arg = i;
+      }
+      if(cre_arg == -1)any = 1;
+      /* client mode */
+      /* sc_dir first searhces /var/tmp and them /tmp
+       * for board files matching sterm unless any is
+       * specified, in which case the first found
+       * board will be joined
+       */
+      if(!create){
+            char* board;
+            if((board = sc_dir(".", any ? NULL : b[cre_arg], 5, ".smbr")) ||
+               (board = sc_dir("/var/tmp", any ? NULL : b[cre_arg], 5, ".smbr")))
+                  /*if(!client(board))rm*/
+                  return !client(board);
+      }
+      if(cre_arg == -1){
             p_usage(*b);
             return 1;
       }
-      _Bool lim_pwd = 0, create = 0;
-      int cre_arg;
-      for(int i = 1; i < a; ++i){
-            if(*b[i] == '-'){
-                  /* pwd mode */
-                  if(b[i][1] == 'p'){
-                        lim_pwd = 1;
-                        continue;
-                  }
-                  /* host/create mode */
-                  if(i < a-1 && b[i][1] == 'C'){
-                        create = 1;
-                        cre_arg = i+1;
-                  }
-            }
-      }
-      if(create){
-            char ext[PATH_MAX] = {0};
-            snprintf(ext, PATH_MAX,
-            (strchr(b[cre_arg], '/') || lim_pwd) ? "%s.smbr" : "/var/tmp/%s.smbr",
-            b[cre_arg]);
-            create_mb(ext);
-            /* create_mb shouldn't return */
-            puts("failed to create mb");
-            return 1;
-      }
-      /* client mode */
-      /* sc_dir first searhces /var/tmp and them /tmp
-       * for board files matching sterm
-       */
-      char* board = sc_dir(".", b[1], 5, ".smbr");
-      if(!board){
-            if(lim_pwd)goto failure;
-            board = sc_dir("/var/tmp", b[1], 5, ".smbr");
-      }
-      if(!board){
-            failure:
-            printf("no board matching %s was found\n", b[1]);
-            return 1;
-      }
-      client(board);
-      return 0;
+      /* if we didn't find a matching board, we'll create it */
+      /* if we've gotten here, !board */
+      char ext[PATH_MAX] = {0};
+      snprintf(ext, PATH_MAX,
+      (strchr(b[cre_arg], '/') || lim_pwd) ? "%s.smbr" : "/var/tmp/%s.smbr",
+      b[cre_arg]);
+      create_mb(ext);
+      /* create_mb shouldn't return */
+      puts("failed to create mb");
+      return 1;
 }
+#if 0
+boards should be able to be created just by ./smb board_name - if it doesn't exist, make it
+be the change you want to see in the world
+.smbr files should be removed automatically when a host cannot connect to it
+client() should try again on next available board after failing
+this will be possible bc .smbr files will be removed if invalid
+#endif
